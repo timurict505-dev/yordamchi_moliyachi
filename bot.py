7ae0d054c1548edf81888d07195d13951607ed29
timurict505-dev <timur.ict505@gmail.com>
2026-04-21 17:12:08 +0500

    added admin ID

--- bot.py
+++ bot.py
@@ -106,7 +106,7 @@ def today_key() -> str:
 
 
 def is_admin(user_id: int) -> bool:
-    return user_id == ADMIN_ID
+    return user_id == 1165988187
 
 
 async def build_category_keyboard(user_id: int, tx_type: str) -> ReplyKeyboardMarkup:
